a78cd5c3e737a3c5c767ff448cb61fbbc53f9404
shobhit0216 <shobhitkumarchaudhary2022@vitbhopal.ac.in>
2025-06-06 19:33:47 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
