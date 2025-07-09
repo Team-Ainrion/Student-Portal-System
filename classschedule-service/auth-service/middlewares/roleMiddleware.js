b1b8824bd8d48c15c3e822876ca6f585cbbf74e4
shobhit0216 <shobhitkumarchaudhary2022@vitbhopal.ac.in>
2025-07-09 18:15:08 +0530

    deleting some files

--- classschedule-service/auth-service/middlewares/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (requiredRole) => {
-  return (req, res, next) => {
-    if (!req.user || req.user.role !== requiredRole) {
-      return res.status(403).json({ msg: "Access denied: Insufficient permissions" });
-    }
-    next();
-  };
-};
--- classschedule-service/course-service/utils/courseUtils.js
+++
@@ -1,7 +0,0 @@
-exports.checkPrerequisites = (required, completed) =>
-  required.every(id => completed.includes(id.toString()));
-
-exports.calculateStudentCredits = async (studentId, CourseModel) => {
-  const enrolledCourses = await CourseModel.find({ studentsEnrolled: studentId });
-  return enrolledCourses.reduce((total, course) => total + course.creditHours, 0);
-};
